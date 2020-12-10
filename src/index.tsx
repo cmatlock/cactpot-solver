1602f9a2b177dc6a4b1ca25ead6995e2ec727ff5
cmatlock <chanel.matlock@gmail.com>
2020-12-10 11:36:25 -0800

    basic layout of board

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,85 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+
+class ScratchOff extends React.Component{
+
+  render(){
+    return (
+      <div className='scratch-off cell'>
+        {/* dropdown of numbers 1-9 */}
+        <select>
+          <option value=''></option>
+          <option value='1'>1</option>
+          <option value='2'>2</option>
+          <option value='3'>3</option>
+          <option value='4'>4</option>
+          <option value='5'>5</option>
+          <option value='6'>6</option>
+          <option value='7'>7</option>
+          <option value='8'>8</option>
+          <option value='9'>9</option>
+        </select>
+      </div>
+    )
+  }
+}
+
+class EstimatedResult extends React.Component{
+  // each restimated result tracks the state of a particular row of cells
+  render(){
+    return (
+      <div className='estimated-result cell'>
+        {/*educated guess*/}
+      </div>
+    )
+  }
+}
+
+class Board extends React.Component{
+  render(){
+    return (
+      <div className='board'>
+        <div className='cactpot-board'>
+          <div className='board-row'>
+            <EstimatedResult/>
+            <EstimatedResult/>
+            <EstimatedResult/>
+            <EstimatedResult/>
+            <EstimatedResult/>
+          </div>
+          <div className='board-row'>
+            <EstimatedResult/>
+            <ScratchOff/>
+            <ScratchOff/>
+            <ScratchOff/>
+            <div className='blank cell'></div>
+          </div>
+          <div className='board-row'>
+            <EstimatedResult/>
+            <ScratchOff/>
+            <ScratchOff/>
+            <ScratchOff/>
+            <div className='blank cell'></div>
+          </div>
+          <div className='board-row'>
+            <EstimatedResult/>
+            <ScratchOff/>
+            <ScratchOff/>
+            <ScratchOff/>
+            <div className='blank cell'></div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Board />
   </React.StrictMode>,
   document.getElementById('root')
 );
